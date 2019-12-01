37f19fee1ac1aa857d71f1eb267df3f297c3a5e5
jenmak <jmak@grubhub.com>
2019-12-01 15:42:36 -0500

    Add semantic ui selection dropdown to select country

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,10 @@ import './App.css';
 import P5Wrapper from 'react-p5-wrapper';
 import countries from './data/countries.json';
 // import Autocomplete from './components/autocomplete.js';
-// import { Dropdown } from 'semantic-ui-react'
+import { Dropdown } from 'semantic-ui-react'
 
 export interface IAppState {
+  countries: any;
   selectedCountry: string;
   year: number;
   sizeFactor: string;
@@ -18,10 +19,13 @@ class App extends React.Component<{}, IAppState> {
   constructor(props: any) {
     super(props);
     this.state = {
+        countries: countries,
         selectedCountry: 'United States of America',
         year: 2018,
         sizeFactor: 'Life Ladder'
     };
+    this.onSizeFactorChange = this.onSizeFactorChange.bind(this);
+    this.onCountryChange = this.onCountryChange.bind(this);
   }
 
   onSizeFactorChange(e: any) {
@@ -30,52 +34,39 @@ class App extends React.Component<{}, IAppState> {
     })
   }
 
-  // const countryOptions = [
-  //   { key: 'af', value: 'af', flag: 'af', text: 'Afghanistan' },
-  //   { key: 'ax', value: 'ax', flag: 'ax', text: 'Aland Islands' },
-  //   { key: 'al', value: 'al', flag: 'al', text: 'Albania' },
-  //   { key: 'dz', value: 'dz', flag: 'dz', text: 'Algeria' },
-  //   { key: 'as', value: 'as', flag: 'as', text: 'American Samoa' },
-  //   { key: 'ad', value: 'ad', flag: 'ad', text: 'Andorra' },
-  //   { key: 'ao', value: 'ao', flag: 'ao', text: 'Angola' },
-  //   { key: 'ai', value: 'ai', flag: 'ai', text: 'Anguilla' },
-  //   { key: 'ag', value: 'ag', flag: 'ag', text: 'Antigua' },
-  //   { key: 'ar', value: 'ar', flag: 'ar', text: 'Argentina' },
-  //   { key: 'am', value: 'am', flag: 'am', text: 'Armenia' },
-  //   { key: 'aw', value: 'aw', flag: 'aw', text: 'Aruba' },
-  //   { key: 'au', value: 'au', flag: 'au', text: 'Australia' },
-  //   { key: 'at', value: 'at', flag: 'at', text: 'Austria' },
-  //   { key: 'az', value: 'az', flag: 'az', text: 'Azerbaijan' },
-  //   { key: 'bs', value: 'bs', flag: 'bs', text: 'Bahamas' },
-  //   { key: 'bh', value: 'bh', flag: 'bh', text: 'Bahrain' },
-  //   { key: 'bd', value: 'bd', flag: 'bd', text: 'Bangladesh' },
-  //   { key: 'bb', value: 'bb', flag: 'bb', text: 'Barbados' },
-  //   { key: 'by', value: 'by', flag: 'by', text: 'Belarus' },
-  //   { key: 'be', value: 'be', flag: 'be', text: 'Belgium' },
-  //   { key: 'bz', value: 'bz', flag: 'bz', text: 'Belize' },
-  //   { key: 'bj', value: 'bj', flag: 'bj', text: 'Benin' },
-  // ]
-  
+  onCountryChange(e: any, d: any) {
+    this.setState({
+      selectedCountry: d.value
+    })
+  }
+
+  dropdownOptions() {
+    let options: any[] = [];
+    this.state.countries[this.state.year].forEach((c: any) => {
+      options.push({
+        text: c.Name,
+        value: c.Name
+      })
+    })
+    return options;
+  }
 
   render () {
     return (
       <div>
         <p>{this.state.selectedCountry}
         </p>
-        {/* <Dropdown
+        <Dropdown
           placeholder='Select Country'
           fluid
           search
           selection
-          options={countries[this.state.year].map((c) => c.Name)}
-        /> */}
-        {/* <Autocomplete
-        options={countries[this.state.year].map((c) => c.Name)}
-        selected={this.state.selectedCountry}/> */}
-
+          onChange={this.onCountryChange}
+          options={this.dropdownOptions()}
+        />
         <div className="form-group">
           <label htmlFor="select2" >Dimension</label>
-          <select value={this.state.sizeFactor} onChange={this.onSizeFactorChange.bind(this)} className="form-control">
+          <select value={this.state.sizeFactor} onChange={this.onSizeFactorChange} className="form-control">
           {Object.keys(DIMENSIONS_MAP).map(option => {
             return <option value={option} key={option} >{option}</option>
           })}
