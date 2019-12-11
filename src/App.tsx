97663204c0aaed4f1c9665b3dfb3d8fdadbab699
jenmak <jmak@grubhub.com>
2019-12-11 10:53:00 -0500

    Testing lighthouse with legend

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,11 @@ import CountryStats from './components/country-stats';
 // import QuestionFlipper from './components/question-flipper';
 import { Icon, Segment, Sidebar } from 'semantic-ui-react'
 // import { render } from 'react-dom';
-import earth from './sketches/earth';
+import legend from './sketches/legend';
 // @ts-ignore
 import P5Wrapper from 'react-p5-wrapper';
 import { connect } from 'react-redux';
-// import { DIMENSIONS_MAP } from './constants/dimensions';
+import { DIMENSIONS_MAP } from './constants/dimensions';
 import { bindActionCreators } from 'redux';
 import * as CountryActions from './actions';
 
@@ -28,13 +28,17 @@ class App extends React.Component<{ actions: any, country: any, dimensions: stri
               <Icon name='question' size='large' circular link />
               <Icon onClick={actions.toggleSidebar} name='chart bar outline' rotated='clockwise' size='large' circular link />
             </div>
-            <div className='globe'>
+            <P5Wrapper
+                sketch={legend}
+                sizeFactor={DIMENSIONS_MAP[dimensions[questionId]].SHORT}
+              />
+            {/* <div className='globe'>
                 <P5Wrapper
                   sketch={earth}
                   selectedCountry={country}
                   sizeFactor={dimensions[questionId]}
                 />
-            </div>
+            </div> */}
             <div className="mainContainer-icons--bottomLeft">
               <Icon name='arrows alternate' size='large' />
             </div>
