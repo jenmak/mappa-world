4dcec00271415cc2254364ad2c1699b85d1297fd
jenmak <jmak@grubhub.com>
2019-12-19 18:10:36 -0500

    Fade in on click

--- src/components/happiness-header.tsx
+++ src/components/happiness-header.tsx
@@ -1,18 +1,30 @@
 import React from 'react';
 
-const HappinessHeader = () => (
-  <div>
-    <div className="bg-purple-darker opacity-75 fixed z-10 w-full h-screen"></div>
-    <div className="p-24 flex flex-col justify-center w-full h-screen fixed z-20">
-      <div>
-        <h2 className="text-2xl md:text-3xl text-white">Visualizing</h2>
-        <h1 className="text-4xl md:text-6xl text-white">World <span className="text-cyan">Happiness</span></h1>
-      </div>
-      <div>
-        <button className="bg-cyan">Click to view</button>
+class HappinessHeader extends React.Component<{}, { visible: boolean }> {
+
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      visible: true
+    }
+  }
+
+  render() {
+    return (
+      <div className={this.state.visible?'fadeIn':'fadeOut'}>
+      <div className="bg-purple-darker opacity-75 fixed z-10 w-full h-screen"></div>
+      <div className="p-24 flex flex-col justify-center w-full h-screen fixed z-20">
+        <div>
+          <h2 className="text-2xl md:text-3xl text-white">Visualizing</h2>
+          <h1 className="text-4xl md:text-6xl text-white">World <span className="text-cyan">Happiness</span></h1>
+        </div>
+        <div>
+          <button className="bg-cyan" onClick={() => this.setState({ visible: false })}>Click to view</button>
+        </div>
       </div>
     </div>
-  </div>
-)
+    )
+  }
+}
 
 export default HappinessHeader;
